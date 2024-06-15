6a3ce75f33ae94d2da74016a3a5fb4bd83794feb
Erin Joy Kramer <ejkramer21@gmail.com>
2024-06-15 11:31:35 -0700

    setup up dirs

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import ReactDOM from 'react-dom';
+import React from 'react';
+import Main from './Main';
+
+function App() {
+  return (
+    <>
+      <h1>Welcome to BlockChain your Health</h1>
+      <div>Upload your medical files securely below</div>
+      <Main/>
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/Main.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+class Main extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      imageURL: '',
+    };
+
+    this.handleUploadImage = this.handleUploadImage.bind(this);
+  }
+
+  handleUploadImage(ev) {
+    ev.preventDefault();
+
+    const data = new FormData();
+    data.append('file', this.uploadInput.files[0]);
+    data.append('filename', this.fileName.value);
+
+    console.log(this.fileName.value)
+
+    fetch('http://localhost:8000/upload', {
+      method: 'POST',
+      body: data,
+    }).then((response) => {
+      response.json().then((body) => {
+        this.setState({ imageURL: `http://localhost:8000/${body.file}` });
+      });
+    });
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleUploadImage}>
+        <div>
+          <input ref={(ref) => { this.uploadInput = ref; }} type="file" />
+        </div>
+      </form>
+    );
+  }
+}
+
+export default Main;
